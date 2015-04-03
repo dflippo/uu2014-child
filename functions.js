92c468836400c41c2fd93e731a3cc6b71d680193
Dan Flippo <djf13@cornell.edu>
2015-04-02 20:08:29 -0400

    Fixed messages for JavaScript instead of CSS

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 /**
 Theme users:
-Below is where you should add any custom CSS that you would like applied to your site.
-This CSS is applied after the CSS from the parent theme.  
-You should look at the style.css from the parent theme and use the same CSS selectors with your modifications.
+Below is where you should add any custom JavaScript that you would like applied to your site.
+This JavaScript is executed after the JavaScript from the parent theme.  
 */
